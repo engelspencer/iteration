fc4ce72bed07a35b2915cfcbd725a1dbcbf2e8d0
engelspencer <sengel@sjnma.org>
2017-10-24 14:13:57 -0500

    not yet complete

---
+++ problem3.py
@@ -0,0 +1,24 @@
+'''
+
+First, import random
+
+Second, make a while loop that continues infinitely as long as the user says the computer has not guessed correctly
+
+
+
+'''
+
+import random
+
+correct = False
+
+guess = 0
+
+cont = 'n'
+
+while (cont != 'y'):
+	cont = input('do you have a number? y/n')
+
+while (correct == False)
+	print (random.randint(1,1000))
+	
